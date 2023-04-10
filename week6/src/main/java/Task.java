c407f72ea10a8f118129fc098729f8a5144b0543
asus <asus@LAPTOP-0G7R35SL>
2023-04-10 16:54:12 +0100

    Added Task Specification in week6 CA.

--- week6/src/main/java/Task.java
+++ week6/src/main/java/Task.java
@@ -1,5 +1,7 @@
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.Objects;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -19,11 +21,79 @@ public class Task {
     public Task(String owner, String description, LocalDate deadline) {
         this.owner = owner;
         this.description = description;
-        if(deadline.equals(LocalDate.now()) || deadline.isAfter(LocalDate.now())){
-            throw new 
+        if(deadline.equals(LocalDate.now()) || deadline.isBefore(LocalDate.now())){
+            throw new DateTimeException("Invalid date time error.");
+        }else{
+            this.deadline = deadline;
         }
-        this.deadline = deadline;
     }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+      if(deadline.equals(LocalDate.now()) || deadline.isBefore(LocalDate.now())){
+            throw new DateTimeException("Invalid date time error.");
+        }else{
+            this.deadline = deadline;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.owner);
+        hash = 67 * hash + Objects.hashCode(this.description);
+        hash = 67 * hash + Objects.hashCode(this.deadline);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Task other = (Task) obj;
+        if (!Objects.equals(this.owner, other.owner)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.deadline, other.deadline)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + "owner: " + owner + ", description: " + description + ", deadline: " + deadline + '}';
+    }
+    
     
     
 }
