23825dbb9068f5a7eec878048684f6ca888db74b
asus <asus@LAPTOP-0G7R35SL>
2023-04-10 16:57:26 +0100

    Added Exception Specification in week6 CA.

---
+++ week6/src/main/java/DuplicateElementException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class DuplicateElementException extends RuntimeException {
+
+    public DuplicateElementException() {
+    }
+
+    public DuplicateElementException(String message) {
+        super(message);
+    }
+    
+}
