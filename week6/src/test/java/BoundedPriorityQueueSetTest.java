d41aac1144057eed3e27eb9517effa7405655cd0
Mercy <d00251752@student.dkit.ie>
2023-04-11 23:35:45 +0100

    Test isFull method in test class.

--- week6/src/test/java/BoundedPriorityQueueSetTest.java
+++ week6/src/test/java/BoundedPriorityQueueSetTest.java
@@ -97,19 +97,71 @@ public class BoundedPriorityQueueSetTest {
     }
 
     /**
-     * Test of isFull method, of class BoundedPriorityQueueSet.
+     * Test of isFull method, of class BoundedPriorityQueueSet with empty que.
      */
     @Test
-    public void testIsFull() {
-        System.out.println("isFull");
-        BoundedPriorityQueueSet instance = null;
+    public void testIsFull_EmptyQue() {
+         BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
         boolean expResult = false;
         boolean result = instance.isFull();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+    
+    /**
+     * Test of isFull method, of class BoundedPriorityQueueSet with populated que.
+     */
+    @Test
+    public void testIsFull_PopulatedQue() {
+        BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
+         instance.add(new Task("Ason", "clean house", "2023-05-11"));
+        instance.add(new Task("Breana", "dance class", "2023-05-11"));
+        boolean expResult = false;
+        boolean result = instance.isFull();
+        assertEquals(expResult, result);
     }
 
+    /**
+     * Test of isFull method, of class BoundedPriorityQueueSet with max size que.
+     */
+    @Test
+    public void testIsFull_maxSizeQue() {
+        BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
+       instance.add(new Task("Ason", "clean house", "2023-05-11"));
+        instance.add(new Task("Breana", "dance class", "2023-05-11"));
+        instance.add(new Task("Caly", "dinner with family", "2023-05-11"));
+        instance.add(new Task("Derek", "grocery shopping", "2023-05-11"));
+        instance.add(new Task("Elton", "dish wash", "2023-05-11"));
+        instance.add(new Task("Felix", "dating", "2023-05-11"));
+        instance.add(new Task("Gloria", "sing lesson", "2023-05-11"));
+        instance.add(new Task("Hanah", "work day", "2023-05-11"));
+        instance.add(new Task("Luna", "cooking class", "2023-05-11"));
+        instance.add(new Task("Melisa", "clinic check", "2023-05-11"));
+        boolean expResult = true;
+        boolean result = instance.isFull();
+        assertEquals(expResult, result);
+    }
+    
+     /**
+     * Test of isFull method, of class BoundedPriorityQueueSet with over size que.
+     */
+    @Test
+    public void testIsFull_overSizeQue() {
+        BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
+       instance.add(new Task("Ason", "clean house", "2023-05-11"));
+        instance.add(new Task("Breana", "dance class", "2023-05-11"));
+        instance.add(new Task("Caly", "dinner with family", "2023-05-11"));
+        instance.add(new Task("Derek", "grocery shopping", "2023-05-11"));
+        instance.add(new Task("Elton", "dish wash", "2023-05-11"));
+        instance.add(new Task("Felix", "dating", "2023-05-11"));
+        instance.add(new Task("Gloria", "sing lesson", "2023-05-11"));
+        instance.add(new Task("Hanah", "work day", "2023-05-11"));
+        instance.add(new Task("Luna", "cooking class", "2023-05-11"));
+        instance.add(new Task("Melisa", "clinic check", "2023-05-11"));
+       assertThrows(IllegalStateException.class, () -> {
+            instance.add(new Task("Nathen", "football", "2023-05-11"));
+        });
+    }
+    
     /**
      * Test of add method, of class BoundedPriorityQueueSet.
      */
