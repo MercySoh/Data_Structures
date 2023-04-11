83f0cc55823cd14514bfb3dadf1627b5a51b529f
asus <asus@LAPTOP-0G7R35SL>
2023-04-11 03:16:28 +0100

    Added BoundedPriorityQueueSet class in week6 CA.

---
+++ week6/src/main/java/BoundedPriorityQueueSet.java
@@ -0,0 +1,97 @@
+
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author Mercy
+ */
+public class BoundedPriorityQueueSet extends LinkedList{
+
+    private static final int INITIAL_CAPACITY = 10;
+    private int capacity;
+
+    public BoundedPriorityQueueSet(int capacity) {
+        this.capacity = capacity;
+    }
+    
+    /**
+     *Get the size of Queue.
+     *
+     * @return size in Queue
+     */
+    @Override
+    public int size() {
+        return super.size(); 
+    }
+
+    /**
+     * Check the Queue is empty or not.
+     *
+     * @return true when Queue is empty else return false
+     */
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty(); 
+    }
+    
+    public boolean isFull() {
+        return size >= capacity;
+    }
+
+    /**
+     *Adds a task to the Queue.
+     *
+     * @param value is use for the specified value(Task)
+     * @return position where task added
+     * @throws IllegalStateException when queue is full
+     * @throws DuplicateElementException when task is duplicate
+     */
+    @Override
+    public boolean add(Task value) {
+         Node newNode = new Node(value);
+        if(isEmpty()){
+            first = newNode;
+            last = newNode;
+        }else if(isFull()){
+            throw new IllegalStateException("Task is full.");
+        }else if(value.equals(this.first.data)){
+           throw new DuplicateElementException("The task is duplicate.");
+        }else{
+            Node current = first.next;
+            Node previous = first;
+            
+            while(!current.data.equals(value)){
+                previous = current;
+                current = current.next;
+            }
+            
+            previous.next = newNode;
+            newNode.next = current;
+        }
+        
+        size++;
+        
+//        for (int i = 0; i < size; i++)
+//                return i;
+//            }
+     
+        return super.add(value); 
+    }
+
+    public Task peek() {
+        if (isEmpty() == true) {
+            throw new NoSuchElementException();
+        }
+        return first.data;
+    }
+    
+    public Task remove() {
+        if (isEmpty() == true) {
+            throw new NoSuchElementException();
+        }
+        Task original = first.data;
+        
+        size--;
+        return original;
+    }
+}
