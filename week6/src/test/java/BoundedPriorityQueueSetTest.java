504a060ddd32e3816002634d3dc20f0ad44048ed
Mercy <d00251752@student.dkit.ie>
2023-04-11 23:28:02 +0100

    Test isEmpty method in test class.

--- week6/src/test/java/BoundedPriorityQueueSetTest.java
+++ week6/src/test/java/BoundedPriorityQueueSetTest.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.Month;
 import org.junit.jupiter.api.Test;
@@ -29,33 +30,70 @@ public class BoundedPriorityQueueSetTest {
     }
 
     /**
-     * Test of size method, of class BoundedPriorityQueueSet with populate queue.
+     * Test of size method, of class BoundedPriorityQueueSet with populate
+     * queue.
      */
     @Test
     public void testSize_PopulatedQue() {
         BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
-        instance.add(new Task("Ason","clean house", "2023-05-11"));
-        instance.add(new Task("Breana","dance class", "2023-05-11"));
-        instance.add(new Task("Caly","dinner with family", "2023-05-11"));
-        instance.add(new Task("Derek","grocery shopping", "2023-05-11"));
-        instance.add(new Task("Elton","dish wash", "2023-05-11"));
+        instance.add(new Task("Ason", "clean house", "2023-05-11"));
+        instance.add(new Task("Breana", "dance class", "2023-05-11"));
+        instance.add(new Task("Caly", "dinner with family", "2023-05-11"));
+        instance.add(new Task("Derek", "grocery shopping", "2023-05-11"));
+        instance.add(new Task("Elton", "dish wash", "2023-05-11"));
         int expResult = 5;
         int result = instance.size();
         assertEquals(expResult, result);
     }
 
     /**
-     * Test of isEmpty method, of class BoundedPriorityQueueSet.
+     * Test of size method, of class BoundedPriorityQueueSet with more than 10
+     * queue.
      */
     @Test
-    public void testIsEmpty() {
-        System.out.println("isEmpty");
-        BoundedPriorityQueueSet instance = null;
+    public void testSize_OversizeQue() {
+        BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
+        instance.add(new Task("Ason", "clean house", "2023-05-11"));
+        instance.add(new Task("Breana", "dance class", "2023-05-11"));
+        instance.add(new Task("Caly", "dinner with family", "2023-05-11"));
+        instance.add(new Task("Derek", "grocery shopping", "2023-05-11"));
+        instance.add(new Task("Elton", "dish wash", "2023-05-11"));
+        instance.add(new Task("Felix", "dating", "2023-05-11"));
+        instance.add(new Task("Gloria", "sing lesson", "2023-05-11"));
+        instance.add(new Task("Hanah", "work day", "2023-05-11"));
+        instance.add(new Task("Luna", "cooking class", "2023-05-11"));
+        instance.add(new Task("Melisa", "clinic check", "2023-05-11"));
+        assertThrows(IllegalStateException.class, () -> {
+            instance.add(new Task("Nathen", "football", "2023-05-11"));
+        });
+    }
+
+    /**
+     * Test of isEmpty method, of class BoundedPriorityQueueSet with empty que.
+     */
+    @Test
+    public void testIsEmpty_EmptyQue() {
+
+        BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
+        boolean expResult = true;
+        boolean result = instance.isEmpty();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of isEmpty method, of class BoundedPriorityQueueSet with populated que.
+     */
+    @Test
+    public void testIsEmpty__PopulatedQue() {
+
+        BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
+        instance.add(new Task("Ason", "clean house", "2023-05-11"));
+        instance.add(new Task("Breana", "dance class", "2023-05-11"));
         boolean expResult = false;
         boolean result = instance.isEmpty();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
     /**
