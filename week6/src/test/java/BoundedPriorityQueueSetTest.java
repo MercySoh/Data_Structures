4e96a81efc875f5718baebd58dee774f5ded013b
Mercy <d00251752@student.dkit.ie>
2023-04-12 00:23:25 +0100

    Test peek method in test class.

--- week6/src/test/java/BoundedPriorityQueueSetTest.java
+++ week6/src/test/java/BoundedPriorityQueueSetTest.java
@@ -6,6 +6,7 @@
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -184,13 +185,14 @@ public class BoundedPriorityQueueSetTest {
     }
 
     /**
-     * Test of add method, of class BoundedPriorityQueueSet with one task in full que.
+     * Test of add method, of class BoundedPriorityQueueSet with one task in
+     * full que.
      */
     @Test
     public void testAdd_TaskInFullQue() {
-        
-       Task value = new Task("Luca", "basketball", "2023-06-17");
-        
+
+        Task value = new Task("Luca", "basketball", "2023-06-17");
+
         BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
         instance.add(new Task("Ason", "clean house", "2023-05-11"));
         instance.add(new Task("Breana", "dance class", "2023-05-11"));
@@ -202,21 +204,21 @@ public class BoundedPriorityQueueSetTest {
         instance.add(new Task("Hanah", "work day", "2023-05-11"));
         instance.add(new Task("Luna", "cooking class", "2023-05-11"));
         instance.add(new Task("Melisa", "clinic check", "2023-05-11"));
-        
+
         assertThrows(IllegalStateException.class, () -> {
             instance.add(value);
         });
-    
+
     }
-    
+
     /**
      * Test of add method, of class BoundedPriorityQueueSet with duplicate task.
      */
     @Test
     public void testAdd_duplicateTask() {
-        
-       Task value = new Task("Luca", "basketball", "2023-06-17");
-        
+
+        Task value = new Task("Luca", "basketball", "2023-06-17");
+
         BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
         instance.add(new Task("Ason", "clean house", "2023-05-11"));
         instance.add(new Task("Caly", "dinner with family", "2023-05-11"));
@@ -225,19 +227,20 @@ public class BoundedPriorityQueueSetTest {
             instance.add(value);
         });
     }
-    
+
     /**
-     * Test of add method, of class BoundedPriorityQueueSet with one task add in last que.
+     * Test of add method, of class BoundedPriorityQueueSet with one task add in
+     * last que.
      */
     @Test
     public void testAdd_TaskInLast() {
-        
-       Task value = new Task("Luca", "basketball", "2023-06-17");
-        
+
+        Task value = new Task("Luca", "basketball", "2023-06-17");
+
         BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
         instance.add(new Task("Ason", "clean house", "2023-05-11"));
         instance.add(new Task("Caly", "dinner with family", "2023-05-30"));
-       
+
         int expResult = 2;
         int result = instance.add(value);
         assertEquals(expResult, result);
@@ -248,17 +251,30 @@ public class BoundedPriorityQueueSetTest {
     }
 
     /**
-     * Test of peek method, of class BoundedPriorityQueueSet.
+     * Test of peek method, of class BoundedPriorityQueueSet with empty que.
      */
     @Test
-    public void testPeek() {
-        System.out.println("peek");
-        BoundedPriorityQueueSet instance = null;
-        Task expResult = null;
+    public void testPeek_emptyQue() {
+        BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
+        assertThrows(NoSuchElementException.class, () -> {
+            instance.peek();
+        });
+
+    }
+
+    /**
+     * Test of peek method, of class BoundedPriorityQueueSet with empty que.
+     */
+    @Test
+    public void testPeek_PopulatedQue() {
+        BoundedPriorityQueueSet instance = new BoundedPriorityQueueSet();
+        instance.add(new Task("Ason", "clean house", "2023-05-11"));
+        instance.add(new Task("Caly", "dinner with family", "2023-05-30"));
+
+        Task expResult = new Task("Ason", "clean house", "2023-05-11");
         Task result = instance.peek();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
     /**
