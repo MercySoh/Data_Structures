01d25a713abd591b9028460caec95217db410eaa
Mercy <d00251752@student.dkit.ie>
2023-04-19 15:09:28 +0100

    Build Appointments class and set up fields.

---
+++ mavenproject4/src/main/java/ApplicationData/Appointments.java
@@ -0,0 +1,22 @@
+package ApplicationData;
+
+import java.time.LocalDate;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class Appointments {
+     private String firstName;
+     private String lastName;
+     private LocalDate dateOfBirth;
+     private String issue;
+     private LocalDate date;
+     private int triageLevel;
+     private String doctorFullName;
+}
